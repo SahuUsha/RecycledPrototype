0066b313d8f3aedbf6575fbeabec857aaec16843
SahuUsha <sahuur@rknec.edu>
2025-02-22 08:12:39 +0530

    Update API URLs to use production endpoint

--- frontend/recycled/src/api.js
+++ frontend/recycled/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import e from "cors";
 
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://recycledprototype.onrender.com/api';
 
 export const api = axios.create({
     baseURL: API_URL,
