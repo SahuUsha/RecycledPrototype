f709e62f27d09745a16efe08cb95d4173c047453
SahuUsha <sahuur@rknec.edu>
2025-02-22 09:03:41 +0530

    Refactor CORS configuration to use environment variable for frontend URL

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,12 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-const allowedOrigins = ["https://prototype-recycled.vercel.app"];  // Your frontend URL
-
 app.use(cors({
-    origin: allowedOrigins,  // Set explicit frontend URL
-    credentials: true,  // Allow cookies
+    origin: process.env.CORS_ORIGIN,  // Frontend URL
+    credentials: true,  // Allow cookies (if using cookies for token)
     methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"],  // Allowed headers
-}));
-
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,  // Frontend URL
-//     credentials: true,  // Allow cookies (if using cookies for token)
-//   }));
+  }));
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,5 +24,6 @@ import eventRouter from './routes/event.router.js'
 
 app.use("/api/users",userRouter)
 app.use("/api/event",eventRouter)
+
 
 export default app;
